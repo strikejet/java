141515220a9d3b5ef66d0141ae8065b1d63cfac7
rohan.paramane <rohan.paramane@sunbeaminfo.com>
2023-04-11 13:02:24 +0530

    Day01 Data added

---
+++ Day01/Program.java
@@ -0,0 +1,10 @@
+class Program {
+
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+
+        // System.in; // Input Stream
+        // System.out; // OutPut Stream
+        // System.err; // Error
+    }
+}
